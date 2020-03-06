ac2d430c7b394f745d9c14bdb71aa89083b6be7a
Gerasimov1991 <svt.triya1991@gmail.com>
2020-03-06 23:16:07 +1000

    first

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -32,6 +32,7 @@
 @include("frontend.includes.header")
 
 <main id="main">
+    this is test
     <div class="rev_slider_wrapper">
         <!-- the ID here will be used in the inline JavaScript below to initialize the slider -->
         <div id="rev_slider_1" class="rev_slider" data-version="5.4.5" style="display:none">
